9d1fd80013e1f8edd65d2b8fcfa58fddce9ed4d4
margaiwangara <margaiwangara@gmail.com>
2020-05-12 19:22:48 +0300

    initial navbar design

--- src/containers/DefaultLayout/DefaultNavbar.js
+++ src/containers/DefaultLayout/DefaultNavbar.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { pixelsToRem } from '@/utils/styles';
-import Logo from '@/components/App/Logo';
+import { pixelsToRem, fontSize } from '@/utils/styles';
+// import Logo from '@/components/App/Logo';
 
 function DefaultNavbar() {
   return (
     <NavbarContainer>
-      <Logo />
+      {/* <Logo /> */}
+      <h3>accumulate</h3>
       <ul className="navbar-right list-unstyled">
+        <li>
+          <Link to="/" className="active">
+            Home
+          </Link>
+        </li>
         <li>
           <Link to="/">Blog</Link>
         </li>
-        <li>
+        {/* <li>
           <Link to="/">About</Link>
         </li>
         <li>
           <Link to="/">Contact</Link>
-        </li>
+        </li> */}
       </ul>
     </NavbarContainer>
   );
@@ -30,7 +36,38 @@ const NavbarContainer = styled.nav`
   grid-template-columns: repeat(2, auto);
   justify-content: space-between;
   align-items: center;
-  padding: ${pixelsToRem(5)} ${pixelsToRem(10)};
+  padding: ${pixelsToRem(15)} ${pixelsToRem(20)};
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.16);
+
+  h3 {
+    color: var(--primaryColor);
+    ${fontSize(22)}
+  }
+
+  figure {
+    width: ${pixelsToRem(115)};
+    padding: 0;
+  }
+
+  .navbar-right {
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    gap: ${pixelsToRem(15)};
+
+    a {
+      color: var(--primaryColor);
+      display: block;
+      border-bottom: solid ${pixelsToRem(2)} transparent;
+      text-transform: uppercase;
+      font-weight: 600;
+      padding: ${pixelsToRem(2.5)} 0;
+      ${fontSize(15)}
+
+      &.active, &:hover {
+        border-bottom-color: var(--primaryColor);
+      }
+    }
+  }
 `;
 
 export default DefaultNavbar;
