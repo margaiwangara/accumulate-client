84f112a91e6c124f09485d0e9e412911d528ed24
margaiwangara <margaiwangara@gmail.com>
2020-09-10 22:21:10 +0300

    modified github actions script based on suggestion for ftp deployment

---
+++ src/views/Pages/About.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import AboutItem from './AboutItem';
+
+function About() {
+  return <AboutItem description="Hello World" />;
+}
+
+export default About;
---
+++ src/views/Pages/AboutItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function AboutItem(props) {
+  const { description } = props;
+  return <p>{description}</p>;
+}
+
+export default AboutItem;
